cd6b200a213c5a9c81fcedbf066784c200522e53
DxExWxExY <DxExWxExY@gmail.com>
2018-01-03 20:52:44 -0700

    UI Colors Changed

--- app/src/main/java/com/abi/dxexwxexy/abi/login.java
+++ app/src/main/java/com/abi/dxexwxexy/abi/login.java
@@ -1,13 +1,32 @@
 package com.abi.dxexwxexy.abi;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+
+    WebView abi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        /*WebView & ToastCode*/
+        Toast.makeText(this, "Version 1.0", Toast.LENGTH_SHORT).show();
+        abi = (WebView) findViewById(R.id.abiViewer);
+        WebSettings abiSettings = abi.getSettings();
+        abiSettings.setJavaScriptEnabled(true);
+        abi.loadUrl("https://ess.abimm.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (abi.canGoBack()) {
+            abi.goBack();
+        }
     }
 }
